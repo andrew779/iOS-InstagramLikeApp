0539e7881a4831f53ddf171f4245f688a16fd3bb
wenzhongzheng <andrew198712@gmail.com>
2017-04-11 13:04:04 -0400

    Initial Commit

---
+++ InstagramLikeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramLikeApp
+//
+//  Created by Wenzhong Zheng on 2017-04-11.
+//  Copyright © 2017 Wenzhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
